3ce79571ab1dded302e2986a1b68de375eb0973b
Diogo Perillo <diogo.perillo@gmail.com>
2021-11-21 09:44:23 -0700

    translate to portuguese

--- src/localization/locales/pt.ts
+++ src/localization/locales/pt.ts
@@ -1,5 +1,52 @@
 export default {
-  homeHeader: 'Inicio',
-  homeTitle: 'Olá, {{name}},',
+  // Buttons General
+  buttonSignOut: 'Sair',
+  buttonSubmit: 'Enviar',
+  // ChangePassword Screen
+  changePasswordHeader: 'Alterar a Senha',
+  // EmailVerification a Screen
+  emailVerificationDescription:
+    "Enviamos um email para '{{email}}' com um link de verificação.",
+  emailVerificationHeader: 'Verifique seu email',
+  emailVerificationSubmit: 'Enviar novo email',
+  emailVerificationTitle: 'Quase pronto!',
+  // Fields
+  fieldEmail: 'Email',
+  fieldName: 'Nome',
+  fieldNewPassword: 'Nova senha',
+  fieldPassword: 'Senha',
+  fieldPhotoFromCamera: 'À partir da camera',
+  fieldPhotoFromLibrary: 'À partir da biblioteca',
+  fieldRepeatPassword: 'Repetir a senha',
+  fieldValidationEmail: 'Email inválido',
+  fieldValidationPassword: 'As senhas precisam ser iguais',
+  fieldValidationRequired: 'Campo obrigatório',
+  // Forgot Password Screen
+  forgotPasswordDescription:
+    'Enviaremos um email com um link para alterar sua senha.',
+  forgotPasswordHeader: 'Esqueci a senha',
+  forgotPasswordSubmit: 'Enviar Link',
+  // Home Screeen
+  homeHeader: 'Início',
   homeSubtitle: 'Bem vindo ao seu app!',
+  homeTitle: 'Olá, {{name}},',
+  // Login Screen
+  loginForgotPassword: 'Esqueceu a senha?',
+  loginHeader: 'Login',
+  loginSubmit: 'Login',
+  // Profile Screen
+  profileHeader: 'Perfil',
+  // Settings Screen
+  settingsHeader: 'Configurações',
+  settingsOptionsChangePasswordDescription: 'Atualize sua senha',
+  settingsOptionsChangePasswordTitle: 'Alterar Senha',
+  settingsOptionsProfileDescription: 'Atualize seu nome e photo',
+  settingsOptionsProfileTitle: 'Perfil',
+  // SignUp Screen
+  signUpHeader: 'Cadastre-se',
+  signUpSubmit: 'Criar conta',
+  // Welcome Screen
+  welcomeLogin: 'Login',
+  welcomeSignUp: 'Criar conta',
+  welcomeTitle: 'Bem Vindo',
 }
